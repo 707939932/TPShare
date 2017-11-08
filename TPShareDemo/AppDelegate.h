17d21a2edc3e532b9d2b475249769c12942517df
th <707939932@qq.com>
2017-11-08 19:13:26 +0800

    Initial Commit

---
+++ TPShareDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TPShareDemo
+//
+//  Created by Me on 2017/11/8.
+//  Copyright © 2017年 Me. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TPShareDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TPShareDemo
+//
+//  Created by Me on 2017/11/8.
+//  Copyright © 2017年 Me. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
